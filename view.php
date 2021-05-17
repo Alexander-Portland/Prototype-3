dd5e4cdba8f1f1939e5bce3194d9839518d5768b
Alexander-Portland <up896020@myport.ac.uk>
2021-05-17 18:28:28 +0100

    view content update

--- view.php
+++ view.php
@@ -1,26 +1,24 @@
 <?php
-//start of connection to database
-$dbh = new PDO("mysql:host=localhost;dbname=demo","root","");
+//first connection to the database
+$con = mysqli_connect('localhost','root','');
+
+mysqli_select_db($con,'demo');
 
 //The ID of the selected post is selected for document extraction
 $id = isset($_GET['post_id'])? $_GET['post_id'] : "";
 
-//Start of fetch query
-$strat = $dbh->prepare("select * from class_posts where post_id=$id");
-$strat->bindParam(1,$id);
-
-//Query execution
-$strat->execute();
-
-//Results of query are extracted
-$row = $strat->fetch();
-
+//the first query checks if the session username and password checkout with an existing student account
+$studentPick = "select * from class_posts where post_id=$id";
+$resultStudent = mysqli_query($con,$studentPick);
+$row = $resultStudent->fetch_assoc();
 //Header of the page that opens document is set to document type
 header('Content-Type: '.$row['mine']);
 
-//Outputted data of extracted document 
+//Outputted data of extracted document
 echo $row['data'];
 
 //end of connection to database
 session_abort();
+
+$numStudent = mysqli_num_rows($resultStudent);
 ?>
